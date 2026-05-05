89e17ce41388fc7be60aa8f9183c70d31b759378
kirill <kirill56gaponenko@gmail.com>
2026-05-05 18:19:22 +0300

    Add removeBook endpoint to delete a book by ISBN

--- src/controller/publisher.controller.js
+++ src/controller/publisher.controller.js
@@ -1,16 +1,11 @@
 import {Author} from "../model/index.js";
 
 export const findPublishersByAuthor = async (req, res) => {
-
     const author = await Author.findByPk(req.params.name);
-    if(!author){
-        return res.status(404).send({error: 'Author not found'});
+    if (!author) {
+        return res.status(404).json({error: `Author with name ${req.params.name} not found`});
     }
-
     const books = await author.getBooks();
     const publishers = [...new Set(books.map(book => book.publisher))];
     return res.json(publishers);
-
-
-
 }
