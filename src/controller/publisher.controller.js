fc33947393245f05fbf9e5a1bd8123b99c591af1
kirill <kirill56gaponenko@gmail.com>
2026-05-07 00:14:31 +0300

    Add removeBook endpoint to delete a book by ISBN

--- src/controller/publisher.controller.js
+++ src/controller/publisher.controller.js
@@ -1,4 +1,6 @@
 import {Author} from "../model/index.js";
+import {sequelize} from "../config/database.js";
+import {QueryTypes} from "sequelize";
 
 export const findPublishersByAuthor = async (req, res) => {
 
@@ -7,8 +9,15 @@ export const findPublishersByAuthor = async (req, res) => {
         return res.status(404).send({error: 'Author not found'});
     }
 
-    const books = await author.getBooks();
-    const publishers = [...new Set(books.map(book => book.publisher))];
+    const publishers = await sequelize.query( `
+    SELECT DISTINCT b.publisher
+    FROM books b 
+    JOIN books_authors ba ON b.isbn = ba.isbn
+    JOIN authors a ON ba.author_name = a.name
+    WHERE a.name = '${req.params.name}'
+    `,{
+        type:QueryTypes.SELECT
+    })
     return res.json(publishers);
 
 
